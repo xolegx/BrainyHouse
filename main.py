f2659c6fb0a6141861a306d233c94955b53c5553
xoleg <xolegx89@gmail.com>
2024-08-05 21:57:56 +0500

    'add_core'

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from core.config import settings
 
 app = FastAPI()
+title = settings.PROJECT_NAME,
+version = settings.PROJECT_VERSION
+
 
 class Item(BaseModel):
     name: str
