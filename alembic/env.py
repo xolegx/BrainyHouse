54f2067a61151d97f07113f709a51210f88621c7
xoleg <xolegx89@gmail.com>
2024-08-11 16:54:32 +0500

    add_schemas

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from core.config import settings
-from db.base_class import Base
+from db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
