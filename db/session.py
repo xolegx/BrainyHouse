106bc9ee0f5ead915234129b008e29597f2c0769
xoleg <xolegx89@gmail.com>
2024-08-12 21:29:19 +0500

    'add_getdb'

--- db/session.py
+++ db/session.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
+from typing import Generator
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
