07a8a48990f531bcd410d83a45ae08ae3bf1bd63
xoleg <xolegx89@gmail.com>
2024-08-24 16:41:24 +0500

    'first_test'

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from typing import Any, Generator
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from db.base import Base
+from db.session import get_db
+from apis.base import api_router
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def start_application():
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def app() -> Generator[FastAPI, Any, None]:
+    Base.metadata.create_all(engine)
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)
